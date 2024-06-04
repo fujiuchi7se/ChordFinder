fb3e5527e654d9f8c524c4a75e3932530a2ab137
fujiuchi7se <120380493+fujiuchi7se@users.noreply.github.com>
2024-06-04 17:07:18 +0900

    Update scrape_test.py

--- scrape_test.py
+++ scrape_test.py
@@ -1,15 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://www.ufret.jp/song.php?data=156149"
+ascale = ["C", "C♭", "C＃", "D", "D♭", "D＃", "E", "E♭", "E＃", "F", "F♭","F＃", "G", "G♭", "G＃", "A", "A♭", "A＃", "B", "B♭", "B＃"]
+rscale = ["I", "♭I", "♯I", "II", "♭II", "♯II", "III", "♭III", "♯III", "IV", "♭IV","♯IV", "V", "♭V", "♯V", "VI", "♭VI", "♯VI", "VII", "♭VII", "♯VII"]
+
+url = "https://gakufu.gakki.me/m/data/DT01794.html"
 html = requests.get(url)
-##html.encoding = 'utf-8'
+html.encoding = 'utf-8'
 bs = BeautifulSoup(html.text, "html.parser")
 
-title = bs.find("span", class_="show_name")
-artist = bs.find("span", class_="show_artist")
-chords = [c.text for c in bs.find_all("rt")]
+title = bs.find("h2", class_="tit").find("span").text.strip("「」")
+artist = bs.find('h2', class_='tit').find('small').text
+print("楽曲名: " + title)
+print("アーティスト: " + artist)
 
-print("楽曲名: " + title.get_text(strip=True))
-print("アーティスト: " + artist.get_text(strip=True))
-print(chords)
+key = "E"
+print("Key: " + key)
+
+chords = [c.text for c in bs.find_all("span", class_="cd_fontpos")]
+for i in chords:
+    #for j in range(len(ascale)):
+        print(i.replace("A", " "))
